99e2b73148d49806a113f17caa38625a2b3cafbd
Wat da fak <juni2484ok@naver.com>
2021-03-03 06:44:19 +0900

    AsyncDataLoader 2.3.4 - Fixed error

--- src/main/kotlin/skywolf46/adl/abstraction/AbstractDataLoader.kt
+++ src/main/kotlin/skywolf46/adl/abstraction/AbstractDataLoader.kt
@@ -3,11 +3,17 @@ package skywolf46.adl.abstraction
 import org.bukkit.Bukkit
 import skywolf46.adl.AsyncDataLoader
 import skywolf46.adl.abstraction.tasks.SchedulerBasedDataSaveTask
-import skywolf46.extrautility.util.nonNull
+import skywolf46.extrautility.util.ifFalse
+import skywolf46.extrautility.util.ifTrue
 import skywolf46.extrautility.util.runNonNull
+import java.util.concurrent.atomic.AtomicBoolean
 
 abstract class AbstractDataLoader<T, F : AbstractDataSnapshot<T>> {
-    protected var task: AbstractDataTask? = null
+    var isLoaded: AtomicBoolean = AtomicBoolean(false)
+        private set
+    val cache: MutableList<AbstractDataLoader<T, F>.() -> Unit> = ArrayList()
+    private var task: AbstractDataTask? = null
+
     abstract fun loadData(data: T)
 
     fun asyncLoadAndExecute(data: T, lambda: () -> Void) {
@@ -21,8 +27,8 @@ abstract class AbstractDataLoader<T, F : AbstractDataSnapshot<T>> {
 
     abstract fun snapshot(): F
 
-    fun forceSave(afterRun: ()-> Unit){
-        runNonNull(task){
+    fun forceSave(afterRun: () -> Unit) {
+        runNonNull(task) {
             stopTimer()
             task = null
         }
@@ -44,4 +50,27 @@ abstract class AbstractDataLoader<T, F : AbstractDataSnapshot<T>> {
         task = null
     }
 
+    protected fun runAllCaches() {
+        val lst: MutableList<AbstractDataLoader<T, F>.() -> Unit> = ArrayList()
+        synchronized(this) {
+            lst.addAll(cache)
+            cache.clear()
+        }
+        for (x in lst)
+            x(this)
+    }
+
 }
+
+@Suppress("UNCHECKED_CAST")
+fun <T, F : AbstractDataSnapshot<T>, S : AbstractDataLoader<T, F>> S.runIfLoaded(unit: S.() -> Unit): S {
+    synchronized(this) {
+        isLoaded.get().ifFalse {
+            cache.add(unit as AbstractDataLoader<T, F>.() -> Unit)
+        }.ifTrue {
+            unit()
+        }
+    }
+    return this
+}
+
